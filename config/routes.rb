4a93fa4b446f1de95c88f8d8d9439cd358ea9c30
neelima1993-1ap <neelujoshi20@gmail.com>
2019-05-13 14:28:26 +0530

    Adding home page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
       root 'authors/sessions#new', as: :unauthenticated_root
     end
   end
+
+
+  resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
